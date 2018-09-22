f0a20432875434bb332a3fb0d795e0f473dfabed
Akinwale Yetunde <akinwaleyetunde1@gmail.com>
2018-09-22 02:20:40 +0100

    Lesson

---
+++ src/lesson10/question1.java
@@ -0,0 +1,15 @@
+package lesson10;
+
+public class question1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			int[] plus = {1,2,3,5,7,11,13,17,19,23,27,2};
+			System.out.println(plus);
+			
+		}
+
+	}
+
+
